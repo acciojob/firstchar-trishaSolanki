c86d72d54b6a5d1ee1f9c59919ad1ce1dbe2a64d
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-12-06 12:03:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 function firstChar(text) {
   // your code here
+	if(!text.trim()){
+		return '';
+		
+	}else{
 	return text.charAt(0);
 }
+}
+
 
 // Do not change the code below
 
+
 const text = prompt("Enter text:");
 alert(firstChar(text));
+
+ 
